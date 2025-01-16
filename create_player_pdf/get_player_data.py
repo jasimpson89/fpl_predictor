d3677326bbaf9d3dd5b5c25e28b771afc53ec7ee
James <jasimpson89@gmail.com>
2025-01-16 01:07:55 +0000

    Added in reading of the fixtures for that year and then merged that with the players so that we can use the fixtures for the data fitting

--- create_player_pdf/get_player_data.py
+++ create_player_pdf/get_player_data.py
@@ -7,7 +7,7 @@ import plotly.express as px
 
  
 class player:
-    def __init__(self, first_name, second_name, path_year, path_year_gw): #TODO expand this to allow for multiple years of data
+    def __init__(self, first_name, second_name, path_year, path_year_gw, path_raw, fixture_path): #TODO expand this to allow for multiple years of data
         self.first_name = first_name
         self.second_name = second_name
         self.name = f"{first_name} {second_name}"
@@ -30,7 +30,25 @@ class player:
         # get now calculate the player pdf
         self.df_player_pdf = self.get_player_pdf()
 
+        # calc player form
+        self.df_player  = self.calc_player_form()
+  
 
+        # read raw player data to get form 
+        self.df_raw = pd.read_csv(path_raw, header=0)
+        self.df_player_raw = self.df_raw.loc[(self.df_raw["first_name"]== self.first_name) & (self.df_raw["second_name"]== self.second_name)]
+
+        # get fixture data
+        self.df_player_fixtures = self.get_fixture_data(fixture_path)
+
+
+    def calc_player_form(self):
+        # calculate the form of a player, but as rolling average from df_player
+        avg = self.df_player['total_points'].rolling(window=5, min_periods=5).mean()
+        self.df_player.loc[:,'rolling_form'] = avg
+        self.df_player.loc[self.df_player['GW'] < 5, 'rolling_form'] = np.nan
+        return self.df_player
+       
     def get_player_data(self):
 
         df_player = (self.df_gw).loc[self.df_gw["name"]== self.name]
@@ -65,11 +83,28 @@ class player:
         )
 
         return df_pdf
+    
+    def get_fixture_data(self, fixture_path):
+        # get the fixture data for the player
+        fixtures_df = pd.read_csv(fixture_path)
+        df_player = self.df_player
+        # Merge player data with fixture data based on kickoff_time and team
+        merged_df = pd.merge(df_player, fixtures_df, how='left', left_on=['kickoff_time', 'opponent_team'], right_on=['kickoff_time', 'team_a'])
+
+        # Drop duplicate columns after merge
+        merged_df = merged_df.loc[:, ~merged_df.columns.duplicated()]
+
+        # sort out fixture difficulty rating working out whether the fixture was at home or away for the player
+        merged_df.loc[merged_df['at_home'] == True, 'fdr'] = merged_df['team_h_difficulty']
+        merged_df.loc[merged_df['at_home'] == False, 'fdr'] = merged_df['team_a_difficulty']
+
+        return merged_df
 
 
-def make_player_obj(player_first_name,player_second_name, path_year, path_year_gw):
+
+def make_player_obj(player_first_name,player_second_name, path_year, path_year_gw, path_player_raw):
 
 
-    player_ = player(player_first_name, player_second_name, path_year, path_year_gw)
+    player_ = player(player_first_name, player_second_name, path_year, path_year_gw, path_player_raw)
     # fig = plot_pdf.plot(player_)
     return player_
--- master.py
+++ master.py
@@ -3,12 +3,13 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotting.plot_players_pdf as plot_players_pdf
 import git
-if __name__ == '__main__':
-    # g = git.cmd.Git('/Fantasy-Premier-League')
+import analysis.form_predictor as form_predictor
+def main():
+    # g = git.cmd.Git('/data_fpl')
     # g.status()
     # g.pull()
 
-    repo = git.Repo('./Fantasy-Premier-League')
+    repo = git.Repo('./data_fpl')
     origin = repo.remote(name='origin')
     origin.pull()
 
@@ -18,25 +19,39 @@ if __name__ == '__main__':
 
 
     # TODO add this to json file, how are we going to look at historical data?
-    path_2023 = "Fantasy-Premier-League/data/2023-24/cleaned_players.csv"
-    path_2023_gw = "Fantasy-Premier-League/data/2023-24/gws/merged_gw.csv"
+    path_2023 = "data_fpl/data/2023-24/cleaned_players.csv"
+    path_2023_gw = "data_fpl/data/2023-24/gws/merged_gw.csv"
+    path_2023_player_raw = "data_fpl/data/2023-24/players_raw.csv"
+    path_2023_fixtures = "data_fpl/data/2023-24/fixtures.csv"
 
     # TODO make a JSON file to import player names and clubs
     player_first_names = ["Mohamed", "Erling"]
     player_second_names = ["Salah", "Haaland"]
 
-
+    colors = ["red", "blue"]
 
     players = {} # dictionary of players
-    fig = make_subplots(rows=1, cols=2, start_cell="top-left")
+    # fig = make_subplots(rows=1, cols=2, start_cell="top-left")
+    fig_form = make_subplots(rows=1, cols=1, start_cell="top-left")
 
 
-    for first_name, second_name in zip(player_first_names, player_second_names):
+    for first_name, second_name, color in zip(player_first_names, player_second_names, colors):
         player_ = get_player_data.make_player_obj(first_name,second_name,
-                                                           path_2023, path_2023_gw)
+                                                           path_2023, path_2023_gw, path_2023_player_raw, path_2023_fixtures)
 
         players[player_.name] = player_
 
-        fig_pdf = plot_players_pdf.plot(player_, fig)
+        print(player_.name)
+        print(player_.df_player["rolling_form"])
 
-    fig_pdf.show()
+        # plot_players_pdf.plot(player_, fig_form, color=color)
+
+        # form_predictor.test_bernoulli_predictor(player_.df_player, player_.name)
+
+    
+
+    # fig_pdf.show()
+    fig_form.show()
+
+main()
+print('END')
